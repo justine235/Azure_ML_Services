81911ca633a12423abe21260c29027b2a9d75d07
justine charley <jcharley@microsoft.com>
2021-07-29 12:44:05 +0200

    new 2807

---
+++ Installation_MLservice-VSC/experiment_folder/script_experience.py
@@ -0,0 +1,22 @@
+# PART 1 - write & save un script d'experience
+from azureml.core import Run
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# Get the experiment run context
+run = Run.get_context()
+
+# load the diabetes dataset
+data = pd.read_csv('data.csv')
+
+# Count the rows and log the result
+row_count = (len(data))
+run.log('observations', row_count)
+
+# Save a sample of the data
+os.makedirs('outputs', exist_ok=True)
+data.sample(100).to_csv("outputs/sample.csv", index=False, header=True)
+
+# Complete the run
+run.complete()
