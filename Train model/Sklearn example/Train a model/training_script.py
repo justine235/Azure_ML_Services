64d29dcf7b4a0e9021349acf007479e375576d42
justine charley <jcharley@microsoft.com>
2021-07-29 15:39:58 +0200

    save 2907

--- Train model/Sklearn example/Train a model/training_script.py
+++ Train model/Sklearn example/Train a model/training_script.py
@@ -3,26 +3,37 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
 
 # Get the experiment run context
 run = Run.get_context()
 
 # Set regularization hyperparameter
 parser = argparse.ArgumentParser()
-parser.add_argument('--reg-rate', type=float, dest='reg_rate', default=0.01)
+parser.add_argument('--reg-rate')
 args = parser.parse_args()
 reg = args.reg_rate
 
 
 # Prepare the dataset
-df = pd.read_csv('./data/train_dataset.csv')
-X, y = df[['Training_Completed','Gender','Fraud_Training']].values, df['EmployeeTargeted'].values
+df = pd.read_csv('data/train_dataset.csv')
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
+df = clean_dataset(df.drop(columns=['Email_Domain']))
+X = df.drop(columns=['EmployeeTargeted']).values
+y = df.filter(['EmployeeTargeted']).values
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
 # Train a logistic regression model
 reg = 0.1
-model = LogisticRegression().fit(X_train, y_train)
+model = RandomForestClassifier(class_weight="balanced").fit(X_train, y_train)
 
 # calculate accuracy
 y_hat = model.predict(X_test)
